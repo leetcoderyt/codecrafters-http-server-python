f0f727f9bc292c576f987a49dbae16b9bf2bd653
LeetCoderYT <leetcoderyt@gmail.com>
2024-03-15 10:43:16 -0700

    Respond with content

--- app/main.py
+++ app/main.py
@@ -4,6 +4,15 @@ BUFFER_SIZE = 1024
 ENCODING = "utf-8"
 
 
+def response(path):
+    if path[0:5] == "/echo":
+        random_string = '/'.join(path.split('/')[2:])
+        return f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(random_string)}\r\n\r\n{random_string}"
+    elif path == "/":
+        return "HTTP/1.1 200 OK\r\n\r\n"
+
+    return "HTTP/1.1 404 Not Found\r\n\r\n"
+
 def main():
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
     client_socket, address = server_socket.accept()
@@ -14,14 +23,7 @@ def main():
     print(f"Received from client: {message}")
 
     path = message.split("\r\n")[0].split(' ')[1]
-
-    if path == "/":
-        response = "HTTP/1.1 200 OK\r\n\r\n"
-        client_socket.sendall(response.encode(ENCODING))
-    else:
-        response = "HTTP/1.1 404 Not Found\r\n\r\n"
-        client_socket.sendall(response.encode(ENCODING))
-
+    client_socket.sendall(response(path).encode(ENCODING))
     client_socket.close()
 
 
