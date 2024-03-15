5a5f26c6e2956d9e767bb44d47f389186a0c3f87
LeetCoderYT <leetcoderyt@gmail.com>
2024-03-15 11:03:36 -0700

    Concurrent connections

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 import socket
+import threading
 
 BUFFER_SIZE = 1024
 ENCODING = "utf-8"
@@ -17,20 +18,37 @@ def response(path, user_agent):
 
     return "HTTP/1.1 404 Not Found\r\n\r\n"
 
-def main():
-    server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    client_socket, address = server_socket.accept()
+def handle_client(client_socket, address):
     print(f"Connection from {address} has been established.")
-
     data = client_socket.recv(BUFFER_SIZE)
     request = data.decode(ENCODING)
     print(f"Received from client: {request}")
 
-    request_split = request.split("\r\n")
-    path = request_split[0].split(" ")[1]
-    user_agent = request_split[2].split(" ")[1]
+    request_lines = request.split("\r\n")
+    path = request_lines[0].split(" ")[1]
+
+    user_agent = None
+    for line in request_lines:
+        if line.startswith("User-Agent:"):
+            user_agent = line.split(" ")[1]
+            break
+
     client_socket.sendall(response(path, user_agent).encode(ENCODING))
     client_socket.close()
+
+def main():
+    server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
+    server_socket.listen()
+
+    try:
+        while True:
+            client_socket, address = server_socket.accept()
+            client_thread = threading.Thread(target=handle_client, args=(client_socket, address))
+            client_thread.start()
+    except KeyboardInterrupt:
+        print("Server is shutting down.")
+    finally:
+        server_socket.close()
 
 
 if __name__ == "__main__":
