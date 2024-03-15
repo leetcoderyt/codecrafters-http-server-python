8da2ef8d46b0ae081d4f82d8466e190e65b781d7
LeetCoderYT <leetcoderyt@gmail.com>
2024-03-15 11:57:29 -0700

    Refactor

--- app/main.py
+++ app/main.py
@@ -9,18 +9,18 @@ ENCODING = "utf-8"
 def response_with_content(content, content_type="text/plain", code=200):
     return f"HTTP/1.1 {code} OK\r\nContent-Type: {content_type}\r\nContent-Length: {len(content)}\r\n\r\n{content}"
 
-def file_response(http_method, path, directory, body):
+def file_response(http_method, path, body, directory):
     file_path = Path(directory) / path.split('/')[-1]
+    content_type = "application/octet-stream"
     if http_method == "POST":
         file_path.write_text(body)
-        return response_with_content('', "application/octet-stream", 201)
-    else:
-        if file_path.is_file():
-            return response_with_content(file_path.read_text(), "application/octet-stream")
+        return response_with_content('', content_type, 201)
+    elif file_path.is_file():
+        return response_with_content(file_path.read_text(), content_type)
 
-def response(http_method, path, user_agent, directory, body):
+def response(http_method, path, user_agent, body, directory):
     if path[0:6] == "/files":
-        response = file_response(http_method, path, directory, body)
+        response = file_response(http_method, path, body, directory)
         if response:
             return response
     elif path[0:5] == "/echo":
@@ -33,24 +33,26 @@ def response(http_method, path, user_agent, directory, body):
 
     return "HTTP/1.1 404 Not Found\r\n\r\n"
 
-def handle_client(client_socket, address, directory):
-    data = client_socket.recv(BUFFER_SIZE)
-    request = data.decode(ENCODING)
-    print(f"Request from {address}:\n{request}")
-
+def parse_request(request):
     request_lines = request.split("\r\n")
-    http_method = request_lines[0].split(" ")[0]
-    path = request_lines[0].split(" ")[1]
+    http_method, path, _ = request_lines[0].split(" ", 2)
     user_agent = None
     for line in request_lines:
         if line.startswith("User-Agent:"):
             user_agent = line.split(" ")[1]
             break
-    body = None
-    if http_method == "POST":
-        body = request_lines[-1]
+    body = request_lines[-1] if http_method == "POST" else None
 
-    client_socket.sendall(response(http_method, path, user_agent, directory, body).encode(ENCODING))
+    return http_method, path, user_agent, body
+
+def handle_client(client_socket, address, directory):
+    data = client_socket.recv(BUFFER_SIZE)
+    request = data.decode(ENCODING)
+    print(f"Request from {address}:\n{request}")
+
+    http_method, path, user_agent, body = parse_request(request)
+
+    client_socket.sendall(response(http_method, path, user_agent, body, directory).encode(ENCODING))
     client_socket.close()
 
 def main():
