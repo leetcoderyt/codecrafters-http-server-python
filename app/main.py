564f56e5457b46a32d844acd928dc1fc6fe9b08c
LeetCoderYT <leetcoderyt@gmail.com>
2024-03-15 10:30:44 -0700

    Respond to HTTP request with a 200 OK response

--- app/main.py
+++ app/main.py
@@ -1,9 +1,21 @@
 import socket
 
+BUFFER_SIZE = 1024
+ENCODING = "utf-8"
+
 
 def main():
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    server_socket.accept() # wait for client
+    client_socket, address = server_socket.accept()
+    print(f"Connection from {address} has been established.")
+
+    data = client_socket.recv(BUFFER_SIZE)
+    message = data.decode(ENCODING)
+    print(f"Received from client: {message}")
+
+    response = "HTTP/1.1 200 OK\r\n\r\n"
+    client_socket.sendall(response.encode(ENCODING))
+    client_socket.close()
 
 
 if __name__ == "__main__":
